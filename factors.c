965407dc46bb36bc216f2d4d202b150614802dca
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-22 23:59:31 +0300

    Update factors.c

--- factors.c
+++ factors.c
@@ -1,23 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    long long int num = 239809320265259;
-    long int factor1 = 2;
-    long int factor2;
-
-    while (num % factor1)
-    {
-        if (factor1 <= num)
-        {
-            factor1++;
-        }
-        else {
-            return (-1);
-        }
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: factors <file>\n");
+        printf("where <file> is a file containing natural numbers to factor.\n");
+        printf("One number per line\n");
+        printf("You can assume that all lines will be valid natural numbers greater than 1\n");
+        printf("You can assume that there will be no empy line, and no space before and after the valid number\n");
+        printf("The file will always end with a new line\n");
+        printf("Output format: n=p*q\n");
+        printf("one factorization per line\n");
+        printf("p and q don’t have to be prime numbers\n");
+        printf("See example\n");
+        printf("You can work on the numbers of the file in the order of your choice\n");
+        printf("Your program should run without any dependency: You will not be able to install anything on the machine we will run your program on\n");
+        printf("Time limit: Your program will be killed after 5 seconds if it hasn’t finish\n");
+        printf("Push all your scripts, source code, etc… to your repository\n");
+        printf("we will only run your executable factors\n");
+        return 1;
     }
 
-    factor2 = num / factor1;
-    printf("%lld = %ld * %ld\n", num, factor2, factor1);
-    return (0);
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        printf("Error opening file %s\n", argv[1]);
+        return 1;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        int num = atoi(line);
+        printf("%d=", num);
+        for (int i = 2; i <= num / i; i++) {
+            while (num % i == 0) {
+                printf("%d*", i);
+                num /= i;
+            }
+        }
+        if (num > 1) {
+            printf("%d", num);
+        } else {
+            printf("\b");
+        }
+        printf("\n");
+    }
+
+    fclose(fp);
+    if (line) {
+        free(line);
+    }
+
+    return 0;
 }
